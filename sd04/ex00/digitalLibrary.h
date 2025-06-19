3251ad5ddb254e7a08a7d82e0fddb242942207c4
X2Borgo <borhiale@gmail.com>
2025-06-19 14:21:59 +0200

    automatic push

--- sd04/ex00/digitalLibrary.h
+++ sd04/ex00/digitalLibrary.h
@@ -6,12 +6,14 @@
 /*   By: alborghi <alborghi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 12:59:10 by alborghi          #+#    #+#             */
-/*   Updated: 2025/06/18 13:00:35 by alborghi         ###   ########.fr       */
+/*   Updated: 2025/06/18 13:03:31 by alborghi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DIGITAL_LIBRARY_H
 #define DIGITAL_LIBRARY_H
+
+#define MAX_SIZE 1000
 
 typedef struct s_book
 {
@@ -22,8 +24,7 @@ typedef struct s_book
 
 typedef struct s_library
 {
-	t_book *books;
-	int size;
+	t_book books[MAX_SIZE];
 	int capacity;
 } t_library;
 
